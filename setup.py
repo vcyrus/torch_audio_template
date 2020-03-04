d8e2a1337c2c5d4d5fdff2eced32d516855aba6c
Cyrus Vahidi <cv300@buxton.eecs.qmul.ac.uk>
2020-03-04 16:29:22 +0000

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bird_activity',
+    version='1.0',
+    description='bird activity classification',
+    author='Cyrus Vahidi',
+    version="0.1",
+    packages=find_packages()
+)
+    
